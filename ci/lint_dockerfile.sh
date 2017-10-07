e7a668b2ee962fab07a6e41a699cdb3abe2a010a
Amit Varman <AmitKumarVarman@gmail.com>
2017-10-08 00:24:18 +0100

    ci config

--- ci/lint_dockerfile.sh
+++ ci/lint_dockerfile.sh
@@ -3,7 +3,7 @@
 set -e -u -x
 
 find PolicyValidationAPI/ -name "Dockerfile*" \
-	-exec echo "Starting linting for policy-validation-api" {} \; \
+	-exec echo "Starting linting for PolicyValidationAPI" {} \; \
 	-exec dockerfile_lint -p -f {} \;
 
 echo "Linted all files with success"
